371bc59de82d25ddd39182f38c36ded104d46c71
Daniel Grieve <daniel.grieve@dubitlimited.com>
2022-01-05 09:30:25 +0000

    Update chapters inline

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -17,6 +17,26 @@ class ChaptersController < ApplicationController
     end
   end
 
+  def edit
+    @chapter = @book.chapters.find(params[:id])
+  end
+
+  def update
+    @chapter = @book.chapters.find(params[:id])
+
+    respond_to do |format|
+      if @chapter.update(chapter_params)
+        format.html { redirect_to book_path(@book) }
+        format.turbo_stream
+      else
+        format.html { render :new }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update(@chapter, partial: "form", locals: { chapter: @chapter })
+        end
+      end
+    end
+  end
+
   private
     def set_book
       @book = Book.find(params[:book_id])
